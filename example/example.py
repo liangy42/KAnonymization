11c6be308bfc5af8e97b28dc8a77b46cfd34668a
liangy42 <39817477+liangy42@users.noreply.github.com>
2022-10-12 21:43:33 +0800

    Add files via upload

--- example/example.py
+++ example/example.py
@@ -1,7 +1,12 @@
 from Anonymization_MainAlgo import AnonymizeByLpGurobiMain
-
+from Anonymization_Greedy_YMILP import AnonymizeByGreedyYMILP
 
 strFile = '../../example/person20_4attr.csv'
 strFileAtt = '../../example/person_4att_sup.txt'
 strFileOut = '../../example/person20_4attr_k3s3.csv'
 AnonymizeByLpGurobiMain(strFile,strFileAtt,3,3, strFileOut)
+
+
+
+
+# AnonymizeByGreedyYMILP('../../example/1k_8attr.txt','../../example/doka_attr_numerical.txt',3, '../../1k8attr_GreedyYMILP_k3.csv')
